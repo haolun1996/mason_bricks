6f9c4ec11bf5dde46bf9fa8f6330f3b066bedae1
haolun <haolun@etctech.com.my>
2022-07-22 11:06:16 +0800

    fixes for hardcoded packagename
    miss out from last commit

--- create_getx_page/__brick__/lib/app/ui/{{module_path.snakeCase()}}/{{name.snakeCase()}}/{{name.snakeCase()}}_page.dart
+++ create_getx_page/__brick__/lib/app/ui/{{module_path.snakeCase()}}/{{name.snakeCase()}}/{{name.snakeCase()}}_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ebric/app/utils/export.dart';
+import 'package:{{{package_name}}}/app/utils/export.dart';
 import 'package:{{{package_name}}}/app/ui/{{module_path.snakeCase()}}/{{name.snakeCase()}}/{{name.snakeCase()}}_controller.dart';
 
 class {{#pascalCase}}{{name}}{{/pascalCase}}Page extends BaseXWidget<{{#pascalCase}}{{name}}{{/pascalCase}}Controller>  {
